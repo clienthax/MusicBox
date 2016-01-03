8afe0773de2f5ba2ef8a822a0576b3acdb28427b
Clienthax <clienthax@gmail.com>
2016-01-03 17:13:54 +0000

    Filters

--- src/main/java/uk/co/haxyshideout/musicbox/commands/ReloadSongsCommand.java
+++ src/main/java/uk/co/haxyshideout/musicbox/commands/ReloadSongsCommand.java
@@ -1,11 +1,11 @@
 package uk.co.haxyshideout.musicbox.commands;
 
-import org.spongepowered.api.text.Text;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.text.Text;
 import uk.co.haxyshideout.musicbox.MusicBox;
 import uk.co.haxyshideout.musicbox.store.SongStore;
 
--- src/main/java/uk/co/haxyshideout/musicbox/eventhandlers/EventHandler.java
+++ src/main/java/uk/co/haxyshideout/musicbox/eventhandlers/EventHandler.java
@@ -11,10 +11,10 @@ import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.block.ChangeBlockEvent;
 import org.spongepowered.api.event.block.InteractBlockEvent;
+import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.event.network.ClientConnectionEvent;
 import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.item.inventory.ItemStack;
-import org.spongepowered.api.item.inventory.ItemStackSnapshot;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.chat.ChatTypes;
 import org.spongepowered.api.text.format.TextColors;
@@ -35,8 +35,9 @@ public class EventHandler {
 
     @Listener
     public void onPlayerLogout(ClientConnectionEvent.Disconnect event) {
-        if(PlaySongCommand.radioSongPlayers.containsKey(event.getTargetEntity().getUniqueId()))
+        if (PlaySongCommand.radioSongPlayers.containsKey(event.getTargetEntity().getUniqueId())) {
             PlaySongCommand.radioSongPlayers.get(event.getTargetEntity().getUniqueId()).setPlaying(false);
+        }
     }
 
     //If the jukebox is broke, stop the song playing
@@ -47,7 +48,7 @@ public class EventHandler {
                 return;
             }
             Location<World> jukeboxLocation = blockSnapshotTransaction.getOriginal().getLocation().get();
-            if(noteBlockPlayers.containsKey(jukeboxLocation)) {
+            if (noteBlockPlayers.containsKey(jukeboxLocation)) {
                 noteBlockPlayers.get(jukeboxLocation).setPlaying(false);
                 noteBlockPlayers.remove(jukeboxLocation);
             }
@@ -60,11 +61,11 @@ public class EventHandler {
             Player player = event.getCause().first(Player.class).get();
             Optional<ItemStack> itemInHand = player.getItemInHand();
             //noinspection ConstantConditions
-            if(itemInHand.isPresent() && itemInHand.get().getItem() == ItemTypes
-                    .JUKEBOX && itemInHand.get().get(Keys.DISPLAY_NAME).isPresent ()) {
+            if (itemInHand.isPresent() && itemInHand.get().getItem() == ItemTypes
+                    .JUKEBOX && itemInHand.get().get(Keys.DISPLAY_NAME).isPresent()) {
                 String itemName = TextSerializers.PLAIN.serialize(itemInHand.get().get(Keys.DISPLAY_NAME).get());
                 //String itemName = Text.legacy().to(itemInHand.get().get(Keys.DISPLAY_NAME).get());
-                if(itemName.equals("Radio")) {
+                if (itemName.equals("Radio")) {
                     event.setCancelled(true);
                     MusicBox.getInstance().getSongStore().sendPlaySongList(player);
                 }
@@ -73,63 +74,60 @@ public class EventHandler {
     }
 
     @Listener
-    public void onInteractWithJukebox(InteractBlockEvent.Secondary event) {
+    public void onInteractWithJukebox(InteractBlockEvent.Secondary event, @First Player player) {
         //If the block interacted with is not a jukebox, return
         //noinspection ConstantConditions
         if (event.getTargetBlock().getState().getType() != BlockTypes.JUKEBOX) {
             return;
         }
-        //Check that a player caused the event
-        if (event.getCause().first(Player.class).isPresent()) {
-            Player player = event.getCause().first(Player.class).get();
-            //Check that the item in the players hand is a record and that it has a custom display name
-            Optional<ItemStack> itemInHand = player.getItemInHand();
-            Location<World> jukeboxLocation = event.getTargetBlock().getLocation().get();
-            //Only insert the disc if the jukebox doesn't have a disc inside it
-            if (!jukeboxLocation.get(Keys.REPRESENTED_ITEM).isPresent()) {
-                if (itemInHand.isPresent() && itemInHand.get().getItem() == ItemTypes.RECORD_CAT && itemInHand.get().get(Keys.DISPLAY_NAME)
-                        .isPresent()) {
-                    event.setCancelled(true);
+        //Check that the item in the players hand is a record and that it has a custom display name
+        Optional<ItemStack> stackInHand = player.getItemInHand();
+        Location<World> jukeboxLocation = event.getTargetBlock().getLocation().get();
+        //Only insert the disc if the jukebox doesn't have a disc inside it
+        if (!jukeboxLocation.get(Keys.REPRESENTED_ITEM).isPresent()) {
+            if (stackInHand.isPresent() && stackInHand.get().getItem() == ItemTypes.RECORD_CAT && stackInHand.get().get(Keys.DISPLAY_NAME)
+                    .isPresent()) {
+                event.setCancelled(true);
 
-                    //Set the record into the tile entity, need to do it this way as the normal insert removes the name tag
-                    jukeboxLocation.offer(Keys.REPRESENTED_ITEM, itemInHand.get().createSnapshot());
+                //Set the record into the tile entity, need to do it this way as the normal insert removes the name tag
+                jukeboxLocation.offer(Keys.REPRESENTED_ITEM, stackInHand.get().createSnapshot());
 
-                    ItemStackSnapshot itemStackSnapshot = jukeboxLocation.get(Keys.REPRESENTED_ITEM).get();
-                    //Remove the disc from the players hand
-                    player.setItemInHand(null);
+                //Remove the disc from the players hand
+                player.setItemInHand(null);
 
-                    //Turn off any players at the blocks location first
-                    if(noteBlockPlayers.containsKey(jukeboxLocation)) {
-                        noteBlockPlayers.get(jukeboxLocation).setPlaying(false);
-                    }
-
-                    //Check that the song exists for the disc name
-                    String songName = TextSerializers.PLAIN.serialize(itemInHand.get().get(Keys.DISPLAY_NAME).get());
-                    //String songName = Texts.legacy().to(itemInHand.get().get(Keys.DISPLAY_NAME).get());
-                    Optional<Song> song = MusicBox.getInstance().getSongStore().getSong(songName);
-                    if(song.isPresent()) {
-                        //Set up the player and play the song
-                        NoteBlockSongPlayer noteBlockSongPlayer = new NoteBlockSongPlayer(song.get());
-                        noteBlockSongPlayer.setNoteBlockLocation(jukeboxLocation);
-                        noteBlockSongPlayer.setAreaMusic(true);
-                        noteBlockSongPlayer.setLooped(true);
-                        noteBlockSongPlayer.setPlaying(true);
-                        noteBlockPlayers.put(jukeboxLocation, noteBlockSongPlayer);
-
-                        //Send the Now Playing message to everyone within 16 blocks
-                        Collection<Entity> playersInRange = player.getWorld().getEntities( entity -> entity instanceof Player
-                                        && entity.getLocation().getPosition().distance(jukeboxLocation.getPosition()) < 16);
-                        for(Entity entity : playersInRange) {
-                            ((Player) entity).sendMessage(ChatTypes.ACTION_BAR, Text.of(TextColors.AQUA, "Now Playing: " + songName));
-                        }
-
-                    }
-                }
-            } else {//If the disc is already in the box just turn off music for that block when ejected
-                if(noteBlockPlayers.containsKey(jukeboxLocation))
+                //Turn off any players at the blocks location first
+                if (noteBlockPlayers.containsKey(jukeboxLocation)) {
                     noteBlockPlayers.get(jukeboxLocation).setPlaying(false);
+                }
+
+                //Check that the song exists for the disc name
+                String songName = TextSerializers.PLAIN.serialize(stackInHand.get().get(Keys.DISPLAY_NAME).get());
+                //String songName = Texts.legacy().to(itemInHand.get().get(Keys.DISPLAY_NAME).get());
+                Optional<Song> song = MusicBox.getInstance().getSongStore().getSong(songName);
+                if (song.isPresent()) {
+                    //Set up the player and play the song
+                    NoteBlockSongPlayer noteBlockSongPlayer = new NoteBlockSongPlayer(song.get());
+                    noteBlockSongPlayer.setNoteBlockLocation(jukeboxLocation);
+                    noteBlockSongPlayer.setAreaMusic(true);
+                    noteBlockSongPlayer.setLooped(true);
+                    noteBlockSongPlayer.setPlaying(true);
+                    noteBlockPlayers.put(jukeboxLocation, noteBlockSongPlayer);
+
+                    //Send the Now Playing message to everyone within 16 blocks
+                    Collection<Entity> playersInRange = player.getWorld().getEntities(entity -> entity instanceof Player
+                            && entity.getLocation().getPosition().distance(jukeboxLocation.getPosition()) < 16);
+                    for (Entity entity : playersInRange) {
+                        ((Player) entity).sendMessage(ChatTypes.ACTION_BAR, Text.of(TextColors.AQUA, "Now Playing: " + songName));
+                    }
+
+                }
+            }
+        } else {//If the disc is already in the box just turn off music for that block when ejected
+            if (noteBlockPlayers.containsKey(jukeboxLocation)) {
+                noteBlockPlayers.get(jukeboxLocation).setPlaying(false);
             }
         }
+
     }
 
 }
